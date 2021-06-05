a36e06e6a537b94d3d24e8c236efa4be3ad53593
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-05 23:18:15 +0530

    added mongoclient

---
+++ node_backend/src/config/mongoose.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+require("dotenv/config");
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_CONNECTION_USER_ID}:${process.env.DB_CONNECTION_PASSWORD}@chatcluster.nwd47.mongodb.net/SimpleChatDB?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then((db) => {
+    console.log("Connected to MonogoDB");
+  })
+  .catch((error) => console.log("Not Connected to MonogoDB due : " + error));
