eefaf0b6879539dfba6e639ea9902142dbb3f7d9
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-07 00:29:04 +0530

    Added UserArgument helper class for ModalRoute

---
+++ flutter_frontend/lib/users/utilities/user_arguments.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_frontend/model/user_model.dart';
+
+// A class that extracts the necessary arguments from the ModalRoute.
+class UserArguments {
+  final UserModel userModel;
+
+  UserArguments({required this.userModel});
+}
