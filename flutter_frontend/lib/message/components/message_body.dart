ea375dddc6d30f70dbd8ab5e4c132ff1e330dbf1
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-07 00:31:33 +0530

    Added MessageBody constructor

--- flutter_frontend/lib/message/components/message_body.dart
+++ flutter_frontend/lib/message/components/message_body.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_frontend/message/components/message_app_bar.dart';
+import 'package:flutter_frontend/model/user_model.dart';
 
 import 'package:flutter_frontend/utilities/size_config.dart';
 import 'package:flutter_frontend/utilities/sized_box_functions.dart';
 
 class MessageBody extends StatelessWidget {
+  final UserModel userModel;
+
+  MessageBody({required this.userModel});
+
   @override
   Widget build(BuildContext context) {
     //initialise SizeConfig
     SizeConfig().init(context);
 
     return SafeArea(
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: SizeConfig.defaultSize! * 3),
-        child: Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [addVerticalSpace(10)],
-          ),
+      child: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [MessageAppBar(userModel: userModel)],
         ),
       ),
     );
