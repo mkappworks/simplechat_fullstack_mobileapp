ec2c8d9bfbf326b5c9b2b0938e5753b9b7e9de76
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-06 14:49:21 +0530

    added size_config

---
+++ flutter_frontend/lib/utilities/size_config.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+//Defined Standard class to get the screenheight and screen width
+// to be used to set the padding, margin and other spaces
+class SizeConfig {
+  static MediaQueryData? _mediaQueryData;
+  static double? screenWidth;
+  static double? screenHeight;
+  static double? blockSizeHorizontal;
+  static double? blockSizeVertical;
+  static double? _safeAreaHorizontal;
+  static double? _safeAreaVertical;
+  static double? safeBlockHorizontal;
+  static double? safeBlockVertical;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData!.size.width;
+    screenHeight = _mediaQueryData!.size.height;
+
+    _safeAreaHorizontal =
+        _mediaQueryData!.padding.left + _mediaQueryData!.padding.right;
+    _safeAreaVertical =
+        _mediaQueryData!.padding.top + _mediaQueryData!.padding.bottom;
+
+    safeBlockHorizontal = (screenWidth! - _safeAreaHorizontal!) / 100;
+    safeBlockVertical = (screenHeight! - _safeAreaVertical!) / 100;
+  }
+}
