e5c4a3f904ddc5a7ca1302941200510b4aef2524
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-06 14:48:50 +0530

    added comments to sized_box_functions

--- flutter_frontend/lib/utilities/sized_box_functions.dart
+++ flutter_frontend/lib/utilities/sized_box_functions.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+//helper functions to create a SizedBox widget with const height or width
 Widget addVerticalSpace(double height){
   return SizedBox(
     height:height
