4f59522ef1d185e8677001dfa3174b71ffa8d32f
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-06 20:50:18 +0530

    formating sized_box_function

--- flutter_frontend/lib/utilities/sized_box_functions.dart
+++ flutter_frontend/lib/utilities/sized_box_functions.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 
 //helper functions to create a SizedBox widget with const height or width
-Widget addVerticalSpace(double height){
-  return SizedBox(
-    height:height
-  );
+Widget addVerticalSpace(double height) {
+  return SizedBox(height: height);
 }
 
-Widget addHorizontalSpace(double width){
-  return SizedBox(
-      width:width
-  );
+Widget addHorizontalSpace(double width) {
+  return SizedBox(width: width);
 }
