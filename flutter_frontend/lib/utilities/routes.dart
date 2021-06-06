13db43770ddf4a15900b705760d44f172072d4c0
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-06 21:00:56 +0530

    updated routes with paths to the routeName screens

--- flutter_frontend/lib/utilities/routes.dart
+++ flutter_frontend/lib/utilities/routes.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/widgets.dart';
 
+import 'package:flutter_frontend/login/login_screen.dart';
+import 'package:flutter_frontend/users/users_screen.dart';
+import 'package:flutter_frontend/message/message_screen.dart';
+
 // Use name route
 // All our routes will be available here
 final Map<String, WidgetBuilder> routes = {
   LoginScreen.routeName: (context) => LoginScreen(),
-  UserScreen.routeName: (context) => UsersScreen(),
+  UsersScreen.routeName: (context) => UsersScreen(),
   MessageScreen.routeName: (context) => MessageScreen(),
 };
