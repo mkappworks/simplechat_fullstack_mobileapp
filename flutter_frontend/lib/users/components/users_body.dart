2d446ec091260bbc09d0871c9a65bbf99145698d
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-06 20:57:43 +0530

    added users_body contains layout of UsersScreen

---
+++ flutter_frontend/lib/users/components/users_body.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+
+import 'package:flutter_frontend/utilities/size_config.dart';
+import 'package:flutter_frontend/utilities/sized_box_functions.dart';
+
+class UsersBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //initialise SizeConfig
+    SizeConfig().init(context);
+
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: SizeConfig.defaultSize! * 3),
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [addVerticalSpace(10)],
+          ),
+        ),
+      ),
+    );
+  }
+}
