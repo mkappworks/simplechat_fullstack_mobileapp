c3a38b29f751cfe0eb24033502ff5083e438f4ad
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-06 23:38:24 +0530

    Added UserCardList to Users

--- flutter_frontend/lib/users/components/users_body.dart
+++ flutter_frontend/lib/users/components/users_body.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_frontend/users/components/users_app_bar.dart';
-import 'package:flutter_frontend/utilities/constants.dart';
 
+import 'package:flutter_frontend/users/components/user_card_list.dart';
+import 'package:flutter_frontend/users/components/users_app_bar.dart';
 
 import 'package:flutter_frontend/utilities/size_config.dart';
-import 'package:flutter_frontend/utilities/sized_box_functions.dart';
 
 class UsersBody extends StatelessWidget {
   @override
@@ -18,7 +17,7 @@ class UsersBody extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             UsersAppBar(),
-            addVerticalSpace(10),
+            UserCardList(),
           ],
         ),
       ),
