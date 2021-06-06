55d227369620f76cfc2234d2a1da485e6a35a471
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-06 23:37:28 +0530

    added UserCardList

---
+++ flutter_frontend/lib/users/components/user_card_list.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_frontend/model/user_model.dart';
+import 'package:flutter_frontend/users/components/user_card.dart';
+import 'package:flutter_frontend/utilities/size_config.dart';
+import 'package:flutter_frontend/utilities/sized_box_functions.dart';
+
+class UserCardList extends StatelessWidget {
+  UserCardList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //initialise SizeConfig
+    SizeConfig().init(context);
+
+    return dummyUserList.isNotEmpty
+        ? Column(children: [
+            ...List.generate(dummyUserList.length,
+                (index) => UserCard(name: dummyUserList[index].name))
+          ])
+        : Column(
+            children: [
+              addVerticalSpace(SizeConfig.defaultSize! * 30),
+              Text(
+                'No Users Found!',
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+            ],
+          );
+  }
+}
