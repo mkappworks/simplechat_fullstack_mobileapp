3b8eb3d822efbd72f287bf774edb6c9af450dcc8
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-06 20:48:55 +0530

    updated size_config with defaultSize

--- flutter_frontend/lib/utilities/size_config.dart
+++ flutter_frontend/lib/utilities/size_config.dart
@@ -6,24 +6,18 @@ class SizeConfig {
   static MediaQueryData? _mediaQueryData;
   static double? screenWidth;
   static double? screenHeight;
-  static double? blockSizeHorizontal;
-  static double? blockSizeVertical;
-  static double? _safeAreaHorizontal;
-  static double? _safeAreaVertical;
-  static double? safeBlockHorizontal;
-  static double? safeBlockVertical;
+  static double? defaultSize;
+  static Orientation? orientation;
 
   void init(BuildContext context) {
     _mediaQueryData = MediaQuery.of(context);
-    screenWidth = _mediaQueryData!.size.width;
-    screenHeight = _mediaQueryData!.size.height;
-
-    _safeAreaHorizontal =
-        _mediaQueryData!.padding.left + _mediaQueryData!.padding.right;
-    _safeAreaVertical =
-        _mediaQueryData!.padding.top + _mediaQueryData!.padding.bottom;
-
-    safeBlockHorizontal = (screenWidth! - _safeAreaHorizontal!) / 100;
-    safeBlockVertical = (screenHeight! - _safeAreaVertical!) / 100;
+    screenWidth = _mediaQueryData?.size.width;
+    screenHeight = _mediaQueryData?.size.height;
+    orientation = _mediaQueryData?.orientation;
+    // On iPhone 11 the defaultSize = 10 almost
+    // So if the screen size increase or decrease then our defaultSize also vary
+    defaultSize = orientation == Orientation.landscape
+        ? screenHeight! * 0.024
+        : screenWidth! * 0.024;
   }
 }
