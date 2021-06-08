c8d3f43d2a848dcf2fc0742e983a45c4b8df4425
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-08 22:40:46 +0530

    Passed TextEditingController for the login inputs

--- flutter_frontend/lib/login/components/login_body.dart
+++ flutter_frontend/lib/login/components/login_body.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_frontend/login/components/login_alert.dart';
 import 'package:flutter_frontend/login/components/login_textfield.dart';
+import 'package:flutter_frontend/service/service_manager.dart';
 import 'package:flutter_frontend/users/users_screen.dart';
 import 'package:flutter_frontend/utilities/constants.dart';
 import 'package:flutter_frontend/utilities/size_config.dart';
 import 'package:flutter_frontend/utilities/sized_box_functions.dart';
 
 class LoginBody extends StatelessWidget {
+  final TextEditingController _emailTextEditingController =
+      TextEditingController();
+  final TextEditingController _nameTextEditingController =
+      TextEditingController();
+  final TextEditingController _ageTextEditingController =
+      TextEditingController();
+  final TextEditingController _genderTextEditingController =
+      TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     //initialise SizeConfig
@@ -27,18 +38,22 @@ class LoginBody extends StatelessWidget {
               LoginTextField(
                 isEmail: true,
                 hintText: kEmailPlaceHolder,
+                textEditingController: _emailTextEditingController,
               ),
               LoginTextField(
                 isEmail: false,
                 hintText: kNamePlaceHolder,
+                textEditingController: _nameTextEditingController,
               ),
               LoginTextField(
                 isEmail: false,
                 hintText: kAgePlaceHolder,
+                textEditingController: _ageTextEditingController,
               ),
               LoginTextField(
                 isEmail: false,
                 hintText: kGenderPlaceHolder,
+                textEditingController: _genderTextEditingController,
               ),
               addVerticalSpace(34),
               TextButton(
