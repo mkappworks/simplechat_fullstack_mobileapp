b668191b5a02f06c4a1f7c7434d49f3203ef7877
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-08 22:57:36 +0530

    Revert "Added /userlist and removed /userinfo"
    This reverts commit 98c56e734225ccd5782686cd9ea603ae88850b44.

--- node_backend/src/route/user.js
+++ node_backend/src/route/user.js
@@ -34,13 +34,13 @@ router.delete("/logout", async (req, res) => {
 });
 
 //get user information
-router.get("/userlist", async (req, res) => {
+router.get("/userinfo", async (req, res) => {
     try {
-      const document = await User.collection.find();
+      const document = await User.findOne({ email: req.body.email });
   
       res.status(200).json(document);
   
-      console.log("/userlist get router success");
+      console.log("/userinfo get router success");
     } catch (error) {
       res.json({ message: error });
     }
