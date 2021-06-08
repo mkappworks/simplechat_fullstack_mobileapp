ec364e254380d3a11ce4e72e96d959cc225eca3a
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-08 22:38:14 +0530

    Added login error alert dialog

---
+++ flutter_frontend/lib/login/components/login_alert.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class LoginAlert extends StatelessWidget {
+  final String title;
+  final String bodyMessage;
+
+  LoginAlert({
+    required this.title,
+    required this.bodyMessage,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(title),
+      content: SingleChildScrollView(
+        child: ListBody(
+          children: <Widget>[
+            Text(bodyMessage),
+          ],
+        ),
+      ),
+      actions: <Widget>[
+        FlatButton(
+          child: Text('Tamam'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
+  }
+}
