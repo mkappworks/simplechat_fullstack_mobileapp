6e8674d66500d1492d940cbd3200faec7940d6e9
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-08 22:44:35 +0530

    Added BottomMessageBox to MessageBody

--- flutter_frontend/lib/message/components/message_body.dart
+++ flutter_frontend/lib/message/components/message_body.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_frontend/message/components/bottom_message_box.dart';
 import 'package:flutter_frontend/message/components/message_app_bar.dart';
-import 'package:flutter_frontend/model/user_model.dart';
+import 'package:flutter_frontend/model/user.dart';
+
+import 'package:flutter_frontend/utilities/size_config.dart';
 
 class MessageBody extends StatelessWidget {
-  final UserModel userModel;
+  final User user;
 
-  MessageBody({required this.userModel});
+  MessageBody({required this.user});
 
   @override
   Widget build(BuildContext context) {
+    //initialise SizeConfig
+    SizeConfig().init(context);
+
     return SafeArea(
       child: Container(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [MessageAppBar(userModel: userModel)],
+          children: [
+            MessageAppBar(userModel: user),
+            Container(
+              height: SizeConfig.defaultSize! * 70,
+            ),
+            BottomMessageBox()
+          ],
         ),
       ),
     );
