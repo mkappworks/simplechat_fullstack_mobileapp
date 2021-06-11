757c4303e9c94ac89462301ed24a126bc30c3f1c
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-11 15:35:45 +0530

    Updated with connectSocket, added disconnectSocket

--- flutter_frontend/lib/controller/helper/socket_helper.dart
+++ flutter_frontend/lib/controller/helper/socket_helper.dart
@@ -1,61 +1,89 @@
+import 'dart:io';
 import 'package:get/get.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
-import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:flutter_frontend/model/user.dart';
 
 import 'package:flutter_frontend/controller/message_controller.dart';
 import 'package:flutter_frontend/controller/user_controller.dart';
 import 'package:flutter_frontend/controller/helper/stream_controller_helper.dart';
-
-import 'package:flutter_frontend/utilities/constants.dart';
+import 'package:flutter_frontend/controller/helper/service_manager.dart';
 
 class SocketHelper {
   MessageController _messageController = Get.find();
   UserController _userController = Get.find();
-  late IO.Socket socket;
-  var id;
+  // static String _serverIP =
+  //     Platform.isAndroid ? 'http://192.168.1.2' : 'http://localhost';
 
-  void connectSocket() async {
-    //get the the id from local storage
-    SharedPreferences pref = await SharedPreferences.getInstance();
-    String id = pref.getString('myID')!;
+  static String _serverIP =
+      Platform.isIOS ? 'http://localhost' : 'http://10.0.2.2';
 
-    //connect to the socket at KBaseURL
-    socket = IO.io(kBaseURL, <String, dynamic>{
-      'transports': ['websocket'],
-      'autoConnect': false,
-    });
+  IO.Socket socket = IO.io('$_serverIP:8080', <String, dynamic>{
+    'transports': ['websocket'],
+    'autoConnect': false,
+  });
 
+  var _logInUserData;
+ 
+  Future<void> connectSocket() async {
+
+    //get the the userdata from local storage
+    _logInUserData = await ServiceManager().getUserInfoFromLocale();
+
+    //connect to the socket
     socket.connect();
 
-    socket.on('connection', (data) {
-      print('connected');
+    //listen to 'connect' socket in the server
+    socket.on('connect', (data) {
 
-      //forward the id of the logined user
-      socket.emit('chatID', {'id': id});
+       //forward the id of the new logged in user to socket
+      if (_logInUserData != null) {
+        socket.emit('userData', {'id': _logInUserData.id});
+      }
 
-      //listen to the receive_message socket to get the message content of other user (i.e. receiver)
+      //listen to the loggedInUser socket to get the users logged in
+      socket.on('loggedInUser', (data) {
+        List<User> _decodedList = usersFromJson(data);
+        List<User> _newList = [];
+
+        //_decodedList contains all the Users in the db
+        //_newList contains all Users in the db except the _logInUserData
+        if (_logInUserData != null) {
+          _newList = _decodedList
+              .where((user) => user.id != _logInUserData.id)
+              .toList();
+        }
+
+        //assign the RxList<User> _usersList = <User>[].obs in the UserController to the _newList
+        _userController.setNewUserListFromSocket(_newList);
+      });
+
+      //listen to the 'receive_message' socket to get the message content of other user (i.e. receiver)
       socket.on('receive_message', (data) {
         var content = data['content'].toString();
 
-        //add the Message to the messageList in Getx MessageController class
+        //add the Message to the messageList in GetX MessageController class
         _messageController.addMessage(message: content, isMy: false);
 
         //set the Last index in the StreamControllerHelper
         StreamControllerHelper()
-            .setLastIndex(_messageController.messageList.length);
+            .setLastIndex(_messageController.getMessageList.length);
       });
     });
   }
 
+ //trigger when logging out from the UsersScreen()
+  Future<void> disconnectSocket() async {
+    if (_logInUserData != null) {
+      socket.disconnect();
+    }
+  }
+
   //trigger when sending a message
-  void sendMessage({required String receiver, required String message}) {
-    _messageController.addMessage(message: message, isMy: true);
-
-    StreamControllerHelper()
-        .setLastIndex(_messageController.messageList.length);
-
+  Future<void> sendMessage(
+      {required String receiver, required String message}) async {
     socket.emit('send_message', {
-      "senderChatID": id,
+      "senderChatID": _logInUserData.id,
       "receiverChatID": receiver,
       "content": message,
     });
