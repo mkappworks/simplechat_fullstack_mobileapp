0aa67a24b310d9201a81a4f8df98c9782694d816
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-11 15:46:56 +0530

    Included SocketController.disconnectFromSocket()

--- flutter_frontend/lib/view/users/components/users_app_bar.dart
+++ flutter_frontend/lib/view/users/components/users_app_bar.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_frontend/controller/user_controller.dart';
 import 'package:get/get.dart';
 import 'package:flutter_svg/svg.dart';
 
 import 'package:flutter_frontend/view/login/login_screen.dart';
+
+import 'package:flutter_frontend/controller/socket_controller.dart';
+import 'package:flutter_frontend/controller/user_controller.dart';
 
 import 'package:flutter_frontend/utilities/size_config.dart';
 import 'package:flutter_frontend/utilities/constants.dart';
 
 class UsersAppBar extends StatelessWidget {
   final UserController _userController = Get.find();
+  final SocketController _socketController = Get.find();
 
   @override
   Widget build(BuildContext context) {
@@ -30,13 +33,18 @@ class UsersAppBar extends StatelessWidget {
               ),
               GestureDetector(
                 onTap: () async {
+                  //this triggers a function that will remove the user from the db
                   await _userController.setLogoutUser();
+                  //this triggers a function that will remove the user from socket room
+                  await _socketController.disconnectFromSocket();
+
                   Navigator.pushReplacementNamed(
                       context, LoginScreen.routeName);
                 },
                 child: Container(
                   width: 30,
                   height: 30,
+                  color: Colors.transparent,
                   child: SvgPicture.asset(kExitSvg),
                 ),
               ),
