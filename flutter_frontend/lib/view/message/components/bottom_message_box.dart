d2c09e32e467c59399ef9ae0442cfb9d6f0e7072
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-11 15:43:31 +0530

    Updated inheritance of BottomMessageBox-HookWidget

--- flutter_frontend/lib/view/message/components/bottom_message_box.dart
+++ flutter_frontend/lib/view/message/components/bottom_message_box.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:get/get.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
+import 'package:flutter_frontend/model/user.dart';
+
+import 'package:flutter_frontend/controller/socket_controller.dart';
 
 import 'package:flutter_frontend/utilities/constants.dart';
 import 'package:flutter_frontend/utilities/size_config.dart';
 
-class BottomMessageBox extends StatelessWidget {
-  
+class BottomMessageBox extends HookWidget {
+  final User user;
+
+  BottomMessageBox({required this.user});
+
   @override
   Widget build(BuildContext context) {
+    final _focusNode = useFocusNode();
+    final _messageTextEditingController = useTextEditingController();
+
     //initialise SizeConfig
     SizeConfig().init(context);
 
@@ -25,14 +37,13 @@ class BottomMessageBox extends StatelessWidget {
                 width: SizeConfig.defaultSize! * 34,
                 child: TextField(
                     textAlign: TextAlign.left,
-                    onChanged: (value) {
-                      // textFieldValue = value;
-                    },
+                    focusNode: _focusNode,
+                    controller: _messageTextEditingController,
                     decoration: kTextFieldDecoration.copyWith(
                         hintText: kMessagePlaceHolder)),
               ),
               GestureDetector(
-                onTap: () => {},
+                onTap: ()  { },
                 child: Container(
                   width: 50,
                   height: 50,
