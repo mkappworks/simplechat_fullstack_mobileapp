40f03fdbce114e968ee3b68bd83ab5b9eee7f86c
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-13 05:48:41 +0530

    Updated Message model

--- flutter_frontend/lib/model/message.dart
+++ flutter_frontend/lib/model/message.dart
@@ -1,35 +1,24 @@
 import 'dart:convert';
 
-List<Message> messagesFromJson(String str) =>
-    List<Message>.from(json.decode(str).map((x) => Message.fromJson(x)));
-
-Message messageFromJson(String str) => Message.fromJson(json.decode(str));
-
-String messagesToJson(List<Message> data) =>
-    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
-
-String messageToJson(Message data) => json.encode(data.toJson());
+List<Message> messagesFromJson(String str) => List<Message>.from(
+    json.decode(str).map((message) => Message.fromJson(message)));
 
 class Message {
   Message({
-    required this.message,
+    this.id,
     required this.isMy,
+    required this.message,
     required this.createdAt,
   });
 
-  final String message;
+  final String? id;
   final bool isMy;
-  final String createdAt;
+  final String message;
+  final DateTime createdAt;
 
   factory Message.fromJson(Map<String, dynamic> json) => Message(
-        message: json["message"] ?? "",
-        isMy: json["isMy"],
-        createdAt: json["createdAt"],
-      );
-
-  Map<String, dynamic> toJson() => {
-        "message": message,
-        "isMy": isMy,
-        "createdAt": createdAt,
-      };
+      id: json["_id"] == null ? null : json["_id"],
+      isMy: json["isMy"],
+      message: json["message"],
+      createdAt: DateTime.parse(json["createdAt"]));
 }
