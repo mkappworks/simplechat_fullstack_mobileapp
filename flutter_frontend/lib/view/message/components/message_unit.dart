66bc8b859e8770198aa1c84f91d1ba2532a7d618
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-14 16:25:29 +0530

    Added Feature to show the selected search message

--- flutter_frontend/lib/view/message/components/message_unit.dart
+++ flutter_frontend/lib/view/message/components/message_unit.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_frontend/utilities/constants.dart';
 import 'package:intl/intl.dart';
 
 import 'package:flutter_frontend/model/message.dart';
 
 class MessageUnit extends StatelessWidget {
   final Message message;
+  final bool isCurrentSearch;
 
-  MessageUnit({required this.message});
+  MessageUnit({required this.message, required this.isCurrentSearch});
 
   @override
   Widget build(BuildContext context) {
@@ -22,12 +24,20 @@ class MessageUnit extends StatelessWidget {
             Container(
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.all(Radius.circular(16)),
-                  color: message.isMy ? Color(0xFF1289FD) : Color(0xFFE5E4EA)),
+                  color: message.isMy
+                      ? kColorGrey.withOpacity(0.5)
+                      : kColorLightBlue),
               margin: EdgeInsets.all(8),
               padding: EdgeInsets.all(8),
-              child: Text(message.message,
-                  style: TextStyle(
-                      color: message.isMy ? Colors.white : Colors.black)),
+              //This shoukd be show color kColorYellow.withOpacity(0.6) when search message is selected
+              child: Container(
+                color: isCurrentSearch
+                    ? kColorYellow.withOpacity(0.6)
+                    : Colors.transparent,
+                child: Text(message.message,
+                    style: TextStyle(
+                        color: message.isMy ? kColorBlack : kColorWhite)),
+              ),
             ),
             Padding(
               padding: EdgeInsets.only(left: 12, right: 12, bottom: 4),
